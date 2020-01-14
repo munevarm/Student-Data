130610b886114bd81e30ad44b082035c29ae6a0f
Andres <munevarm@sheridancollege.ca>
2020-01-14 18:39:16 -0500

    Initial commit of StudentData.java

---
+++ src/namebackwards/Main.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package namebackwards;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //System.out.println("Type in: ");
+        
+//        char letter [] = new char [word.length()];
+//        for( int i = word.length()-1 ; i >= 0 ; i--){
+//            letter[i] = word.charAt(i);
+//            System.out.println(letter[i]);
+//        }
+//        System.out.println(letter);
+
+//
+//        StudentData names = new StudentData();
+//        String student [] = new String[5];
+//        
+//        for(int i = 0; i<student.length;i++){
+//            System.out.print("Enter name: ");
+//            names.setName(input.nextLine());
+//            student [i] = names.getName();
+//            
+//        }
+//        for(int i = 0; i<student.length;i++){
+//            System.out.println(student[i]);
+//        }
+        
+//      Class 2
+        StudentData [] student = new StudentData[2];
+        
+        for (int i = 0; i < student.length ; i++) {
+            
+            student[i] = new StudentData();
+            
+            System.out.println("Type id:");
+            student[i].setId(input.nextInt());
+            
+            System.out.println("Type name:");
+            student[i].setName(input.next());
+            
+        }
+        
+        for(StudentData students: student){
+        System.out.println("Name: " + students.getName() + " - ID: " + students.getId());
+        }
+    }
+}
---
+++ src/namebackwards/StudentData.java
@@ -0,0 +1,52 @@
+
+package namebackwards;
+
+public class StudentData {
+    
+    private int id;
+    private String name;
+    private String address;
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param adress the address to set
+     */
+    public void setAddress(String adress) {
+        this.address = adress;
+    }
+    
+}
